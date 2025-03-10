8dbd37769bfc8bbc865c9fac909c162f531166ca
TODAISY <91897155+todaisy@users.noreply.github.com>
2025-03-11 01:47:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ from parser import parser_html, parser_xml
 
 app = Celery('tasks', broker='redis://localhost:6379/0')
 
+app.conf.update(
+    task_always_eager=True,
+    task_eager_propagates=True
+)
+
 
 class HtmlParserTask(app.Task):
     name = 'html_parser'
 
-    def run_html(self, url):
+    def run(self, url):
         links = parser_html(url)
 
         for link in links:
-            app.send_task('xml_parser', args=(link,))
-
+            # app.send_task('xml_parser', args=[link])  # для redis
+            app.tasks['xml_parser'].apply(args=[link])
 
 class XmlParserTask(app.Task):
     name = 'xml_parser'
 
-    def run_xml(self, url):
+    def run(self, url):
         parser_xml(url)
 
 
@@ -25,9 +30,7 @@ app.register_task(HtmlParserTask())
 app.register_task(XmlParserTask())
 
 
-if __name__ == '__main__':
-    HtmlParserTask().apply_async(args=('https://zakupki.gov.ru/epz/order/extendedsearch/results.html?fz44=on&pageNumber=1',))
-    HtmlParserTask().apply_async(args=('https://zakupki.gov.ru/epz/order/extendedsearch/results.html?fz44=on&pageNumber=2',))
-
+HtmlParserTask().apply_async(args=('https://zakupki.gov.ru/epz/order/extendedsearch/results.html?fz44=on&pageNumber=1',))
+HtmlParserTask().apply_async(args=('https://zakupki.gov.ru/epz/order/extendedsearch/results.html?fz44=on&pageNumber=2',))
 
 # celery -A main worker --loglevel=info
